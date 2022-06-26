ed9f11c8f66863c52d12fd4366714965b7024907
Joseph Rumpe <jrumpe@gmail.com>
2022-06-27 01:49:22 +0300

    first commit

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -0,0 +1,55 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> listAll () {
+        return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRoles () {
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save (User user) {
+        encodePassword(user);
+        userRepo.save(user);
+    }
+
+    private void encodePassword (User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+    }
+
+    public boolean isEmailUnique (String email) {
+        User userByEMail = userRepo.getUserByEmail(email);
+
+        return userByEMail == null;
+    }
+
+    public User get (Integer id) throws UserNotFoundException {
+        try{
+            return userRepo.findById(id).get();
+        } catch (NoSuchElementException ex){
+            throw new UserNotFoundException("Could not find any User with ID " + id);
+        }
+    }
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -0,0 +1,98 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateNewUserWithOneRole () {
+        Role roleAdmin = entityManager.find(Role.class, 1);
+        User userRumpe = new User("jrumpe@gmail.com", "12345", "Joseph", "Rumpe");
+        userRumpe.addRole(roleAdmin);
+        User savedUser = repo.save(userRumpe);
+
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateNewUserWithTwoRoles () {
+        User userRavi = new User("ravi@gmail.com", "ravi2020", "Ravi", "Kumar");
+        Role roleEditor = new Role(3);
+        Role roleAssistant = new Role(5);
+
+        userRavi.addRole(roleEditor);
+        userRavi.addRole(roleAssistant);
+
+        User savedUser = repo.save(userRavi);
+
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAllUsers () {
+        Iterable<User> listUsers = repo.findAll();
+        listUsers.forEach(user -> System.out.println(user));
+
+    }
+
+    @Test
+    public void testGetUserById () {
+        User userjrumpe = repo.findById(1).get();
+        System.out.println(userjrumpe);
+        assertThat(userjrumpe).isNotNull();
+    }
+
+    @Test
+    public void testUpdateUserDetails () {
+        User user = repo.findById(1).get();
+        user.setEnabled(true);
+        user.setEmail("jrumpe@datum.co.ke");
+
+        repo.save(user);
+
+    }
+
+    @Test
+    public void testUpdateUserRole () {
+        User ann = repo.findById(27).get();
+        Role roleEditor = new Role(3);
+        Role roleSaleperson = new Role(2);
+        ann.getRoles().add(roleEditor);
+        ann.getRoles().add(roleSaleperson);
+
+        repo.save(ann);
+    }
+
+    @Test
+    public void testDeleteUser () {
+        Integer userId = 2;
+        repo.deleteById(userId);
+    }
+
+    @Test
+    public void testGetUserByEmail () {
+        String email = "ravi@gmail.com";
+        User user = repo.getUserByEmail(email);
+
+        assertThat(user).isNotNull();
+    }
+}
