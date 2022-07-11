672ceb4f4af5ef1b4955e55e89dd6e1d16ec48ab
Joseph <jrumpe@gmail.com>
2022-07-11 19:13:18 +0300

    Commit Code Logout Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -41,7 +41,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure (HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("email").permitAll();
+        http.authorizeRequests().
+                anyRequest().
+                authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 
     @Override
