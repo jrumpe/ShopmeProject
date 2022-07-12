5db72ed4f8b55c7a887f987c1fd16d9526d0659d
Joseph <jrumpe@gmail.com>
2022-07-12 23:27:17 +0300

    Commit Code User Authorization Basics.

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure (HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("email").permitAll().and().logout().permitAll().and().rememberMe().key("abcDefgHijKlmnOpqrs_1234567890").tokenValiditySeconds(7 * 24 * 60 * 60);
+        http.authorizeRequests().antMatchers("/users/**").hasAnyAuthority("Admin").anyRequest().authenticated().and().formLogin().loginPage("/login").usernameParameter("email").permitAll().and().logout().permitAll().and().rememberMe().key("abcDefgHijKlmnOpqrs_1234567890").tokenValiditySeconds(7 * 24 * 60 * 60);
     }
 
     @Override
