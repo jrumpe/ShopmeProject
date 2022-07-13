b6032a022107312cdcb5b291c8c37eb1db7d3cd8
Joseph <jrumpe@gmail.com>
2022-07-13 07:24:02 +0300

    Commit Code Category Entity and Repository.

--- ShopmeCommon/src/main/java/com/shopme/common/ShopmeCommonApplication.java
+++ ShopmeCommon/src/main/java/com/shopme/common/ShopmeCommonApplication.java
@@ -1,4 +1,4 @@
-package com.shopme.common.entity;
+package com.shopme.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
