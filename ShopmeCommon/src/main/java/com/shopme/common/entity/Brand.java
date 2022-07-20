faf7ccc71ecb29e0b5a9c46bf7938ac5f4ca1a85
Joseph <jrumpe@gmail.com>
2022-07-20 23:20:27 +0300

    Commit Code Brand Form.

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -25,12 +25,22 @@ public class Brand {
     private String logo;
 
     @ManyToMany()
-    @JoinTable(
-            name = "brands_categories",
-            joinColumns = @JoinColumn(name = "brand_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
+    @JoinTable(name = "brands_categories", joinColumns = @JoinColumn(name = "brand_id"), inverseJoinColumns = @JoinColumn(name =
+            "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    public Brand () {
+    }
+
+    public Brand (Integer id) {
+        this.id = id;
+    }
+
+    public Brand (String name, String logo) {
+        this.name = name;
+        this.logo = logo;
+    }
+
 
     public Integer getId () {
         return id;
@@ -62,5 +72,20 @@ public class Brand {
 
     public void setCategories (Set<Category> categories) {
         this.categories = categories;
+    }
+
+    public void addCategory (Category category) {
+        this.categories.add(category);
+    }
+
+    @Override
+    public String toString () {
+        return "Brand{" + "id=" + id + ", name='" + name + '\'' + ", categories=" + categories + '}';
+    }
+
+    @Transient
+    public String getLogoPath () {
+        if (this.id == null) return "/images/image-thumbnail.png";
+        return "/brand-logos/" + this.id + "/" + this.logo;
     }
 }
