fbd613573567e998da36582b82a838eca1aa71d5
Joseph <jrumpe@gmail.com>
2022-07-20 19:14:46 +0300

    Commit Code Brand Entity and Repository.

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022.
+ * Joseph Rumpe (jrumpe@gmail.com)
+ * All rights reserved.
+ */
+
+package com.shopme.admin.brand;
+
+import com.shopme.common.entity.Brand;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
